b4f3b763b73131fb0b1513bcc4795d86604005a6
davide mezzetti <davide.mezzetti@hotmail.it>
2021-03-28 18:37:30 +0200

    use fixture naming convenction

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,9 +1,8 @@
 describe('App initialization ', () => {
   it('Displays todos from API on load', () => {
     cy.server()
-    cy.fixture('todos').then(todos => {
-      cy.route('GET', '/api/todos', todos)
-    })
+
+    cy.route('GET', '/api/todos', 'fixture.todos')
 
     cy.visit('/')
     cy.get('.todo-list li').should('have.length', 4)
