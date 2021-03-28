8232e0fdcb809f44fbb83a40ac176e5902b2d0e7
davide mezzetti <davide.mezzetti@hotmail.it>
2021-03-28 18:35:45 +0200

    use fixture to import json data

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,29 +1,10 @@
 describe('App initialization ', () => {
-  const todos = [
-    {
-      id: 1,
-      isComplete: false,
-      name: 'Capra'
-    },
-    {
-      id: 2,
-      isComplete: false,
-      name: 'C'
-    },
-    {
-      id: 3,
-      isComplete: false,
-      name: 'Ciccioo'
-    },
-    {
-      id: 4,
-      isComplete: false,
-      name: 'fourth'
-    }
-  ]
   it('Displays todos from API on load', () => {
     cy.server()
-    cy.route('GET', '/api/todos', todos)
+    cy.fixture('todos').then(todos => {
+      cy.route('GET', '/api/todos', todos)
+    })
+
     cy.visit('/')
     cy.get('.todo-list li').should('have.length', 4)
   })
