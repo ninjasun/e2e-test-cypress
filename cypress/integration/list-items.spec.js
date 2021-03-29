54e04f78fcbb6b8c88c53e85ab1ad855b61e883f
davide mezzetti <davide.mezzetti@hotmail.it>
2021-03-29 11:56:56 +0200

    test PUT

--- cypress/integration/list-items.spec.js
+++ cypress/integration/list-items.spec.js
@@ -1,3 +1,5 @@
+const { _ } = Cypress
+
 describe('List item behaviur', () => {
   it('Deletes an item', () => {
     cy.server()
@@ -17,5 +19,31 @@ describe('List item behaviur', () => {
 
     cy.wait('@delete')
     cy.get('.todo-list li').should('have.length', 3)
+  })
+
+  it.only('Marks an item complete', () => {
+    cy.server()
+    cy.seedAndVisit()
+    cy.fixture('todos').then(todos => {
+      const target = todos[0]
+      cy.route({
+        method: 'PUT',
+        url: `/api/todos/${target.id}`,
+        request: _.merge(target, { isCompleted: true })
+      }).as('update')
+    })
+    cy.get('.todo-list li')
+      .first()
+      .as('first-todo')
+
+    cy.get('@first-todo')
+      .find('.toggle')
+      .as('checkbox')
+
+    cy.get('@checkbox').click()
+    cy.wait('@update')
+    cy.get('@checkbox').should('be.checked')
+    cy.get('@first-todo').should('have.class', 'completed')
+    cy.get('.todo-count').should('contain', 3)
   })
 })
